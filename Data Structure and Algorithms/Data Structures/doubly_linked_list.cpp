1bed0003ca9da2e7a7db5969d2729f709a428b79
Sid200026 <siddharthsingharoy@gmail.com>
2019-11-25 11:32:00 +0530

    Updated doubly linked list

--- Data Structure and Algorithms/Data Structures/doubly_linked_list.cpp
+++ Data Structure and Algorithms/Data Structures/doubly_linked_list.cpp
@@ -16,6 +16,8 @@ public:
     doubly_linked_list();
     void traverse();
     void addAtStart(int);
+    int deletepos(int);
+    void addAtPos(int, int);
 };
 
 doubly_linked_list::doubly_linked_list()
@@ -24,17 +26,56 @@ doubly_linked_list::doubly_linked_list()
     cur = head;
 }
 
+void doubly_linked_list::addAtPos(int data, int pos)
+{
+    int position = 0;
+    cur = head;
+    while(pos!=position)
+    {
+        position++;
+        cur = cur->next;
+        if(cur==NULL)
+        {
+            cout<<"Out of bounds";
+            return;
+        }
+    }
+    node *temp = new node;
+    temp->data = data;
+    temp->prev = cur;
+    temp->next = cur->next;
+    cur->next->prev = temp;
+    cur->next = temp;
+}
+
 void doubly_linked_list::traverse()
 {
     cur = head;
-    if(cur==NULL)
-        return;
-    while(cur->next!=NULL)
+    while(cur!=NULL)
     {
         cout<<cur->data<<" ";
         cur = cur->next;
     }
-    cout<<cur->data<<"\n";
+
+}
+
+int doubly_linked_list::deletepos(int pos)
+{
+    cur = head;
+    int position = 0;
+    while(pos!=position)
+    {
+        position++;
+        cur = cur->next;
+        if(cur == NULL)
+        {
+            cout<<"Out of bounds";
+            return;
+        }
+    }
+    // cur is the one that I will delete
+    cur->prev->next = cur->next;
+    delete cur;
 }
 
 void doubly_linked_list::addAtStart(int item)
