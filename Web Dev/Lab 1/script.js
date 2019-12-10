2d1559f27157d412a0564f99bd37e2c84528e564
Sid200026 <siddharthsingharoy@gmail.com>
2019-12-10 17:18:51 +0530

    Completed Registration Form

--- Web Dev/Lab 1/script.js
+++ Web Dev/Lab 1/script.js
@@ -7,4 +7,50 @@ function newPage(){
     second.style.display = "contents";
     fourth.style.display = "none";
     third.style.display = "flex";
+    const progress = document.getElementsByClassName("actual")[0];
+    let width = progress.style.width;
+    const final = 50;
+    const time = setInterval(updateBar, 10);
+    function updateBar() {
+        if(width===final)
+            clearInterval(time);
+        else{
+            width++;
+            progress.style.width = width + "%";
+        }
+    }
+}
+const entire = document.getElementsByClassName("final")[0];
+const result = document.getElementsByClassName("aftersubmit")[0];
+function finalSubmit() {
+    entire.style.display = "none";
+    result.style.display = "flex";
+}
+
+function updateBar(final) {
+    const progress = document.getElementsByClassName("actual")[0];
+    let width = progress.style.width;
+    let val = "";
+    for(let i=0;i<width.length;i++)
+    {
+        if(width[i]=="%")
+            break;
+        val+=width[i];
+    }
+    const time = setInterval(updateBar, 10);
+    function updateBar() {
+        if(val==final)
+        {
+            if(final==100)
+            {
+                const quote = document.getElementsByClassName("fixme")[0];
+                quote.innerHTML = "Completed";
+            }    
+            clearInterval(time);
+        }
+        else{
+            val++;
+            progress.style.width = val + "%";
+        }
+    }
 }
