5094ea3f84f06054d86de7f53595c29d20b3817c
Sid200026 <siddharthsingharoy@gmail.com>
2019-12-22 23:05:50 +0530

    Added enum in py

---
+++ Python/enumerator.py
@@ -0,0 +1,26 @@
+import enum
+class EnumExample(enum.Enum):
+    noPerm = "Insufficient priviledges"
+    notSame = "Values are not same"
+    permReq = "Ask admin for permission"
+
+print(EnumExample.noPerm)
+# EnumExample.noPerm
+
+print(repr(EnumExample.noPerm))
+# <EnumExample.noPerm: 'Insufficient priviledges'>
+
+print(EnumExample.noPerm.name)
+# noPerm
+
+print(EnumExample.noPerm.value)
+# Insufficient priviledges
+
+# Enum cannot have two attributes with the same name
+
+# If we want all the values to be unique
+# from enum import unique
+# @unique
+# class .....
+
+
